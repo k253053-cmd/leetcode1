5c7fd44f0b2ac001f2c0afc1aead767901c20d0e
Taha Javed <k253053@nu.edu.pk>
2025-11-14 09:54:02 +0500

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,10 +1,11 @@
 int* shuffle(int* nums, int numsSize, int n, int* returnSize){
-int* res = malloc(sizeof(int) * numsSize);
-*returnSize = numsSize;
-int idx = 0;
-for(int i = 0; i < n; i++){
-res[idx++] = nums[i];
-res[idx++] = nums[i + n];
+int* res=malloc(sizeof(int) * numsSize);
+*returnSize=numsSize;
+int idx=0;
+for(int i= 0;i<n;i++){
+res[idx++]=nums[i];
+res[idx++]=nums[i + n];
 }
 return res;
 }
+
