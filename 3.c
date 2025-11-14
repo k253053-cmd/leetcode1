b8e08ae07b15fa0ca4bf43c53ecfc5ba2886b4e2
Taha Javed <k253053@nu.edu.pk>
2025-11-14 09:46:42 +0500

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,12 @@
+int* transformArray(int* nums, int numsSize, int* returnSize){
+int* res = malloc(sizeof(int) * numsSize);
+*returnSize = numsSize;
+int idx = 0;
+for(int i = 0; i < numsSize; i++){
+if(nums[i] % 2 == 0) res[idx++] = nums[i];
+}
+for(int i = 0; i < numsSize; i++){
+if(nums[i] % 2 != 0) res[idx++] = nums[i];
+}
+return res;
+}
