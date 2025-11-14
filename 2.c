23504d347068b1554869a7936c9e36fd5fd3ecc9
Taha Javed <k253053@nu.edu.pk>
2025-11-14 09:54:38 +0500

    Refactor runningSum function formatting

--- 2.c
+++ 2.c
@@ -1,9 +1,10 @@
 int* runningSum(int* nums, int numsSize, int* returnSize){
 int* res = malloc(sizeof(int) * numsSize);
-*returnSize = numsSize;
-res[0] = nums[0];
-for(int i = 1; i < numsSize; i++){
-res[i] = res[i - 1] + nums[i];
+*returnSize=numsSize;
+res[0]=nums[0];
+for(int i=1;i<numsSize; i++){
+res[i]=res[i-1]+nums[i];
 }
 return res;
 }
+
