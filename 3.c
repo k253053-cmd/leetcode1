00419f8e29a7444a9df5a64e3d325c282e7b187a
Taha Javed <k253053@nu.edu.pk>
2025-11-14 09:55:29 +0500

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,12 +1,13 @@
 int* transformArray(int* nums, int numsSize, int* returnSize){
 int* res = malloc(sizeof(int) * numsSize);
-*returnSize = numsSize;
-int idx = 0;
-for(int i = 0; i < numsSize; i++){
-if(nums[i] % 2 == 0) res[idx++] = nums[i];
+*returnSize=numsSize;
+int idx=0;
+for(int i=0;i<numsSize; i++){
+if(nums[i]%2==0)res[idx++]=nums[i];
 }
-for(int i = 0; i < numsSize; i++){
-if(nums[i] % 2 != 0) res[idx++] = nums[i];
+for(int i=0; i<numsSize; i++){
+if(nums[i] %2!=0) res[idx++] = nums[i];
 }
 return res;
 }
+
